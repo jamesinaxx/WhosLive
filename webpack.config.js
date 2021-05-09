aaa6abc75dba001354e37d410d7fd0dbc629cfa8
jamesinaxx <grassojames5@gmail.com>
2021-05-09 17:40:57 +1000

    Added some basic files

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			// First Rule
+			{
+				test: /\.[jt](s|sx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+
+			// Second Rule
+			{
+				test: /\.s[ac]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'public/index.html',
+		}),
+		new CopyPlugin({
+			patterns: [{ from: 'public/chrome' }],
+		}),
+	],
+};
