9ce7cdb6259f84270595e79b4dd15f480007a800
jamesinaxx <grassojames5@gmail.com>
2021-05-10 12:18:02 +1000

    Added following channels for me

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,33 @@ import { ButtonGroup, Button } from '@material-ui/core';
 import SettingsIcon from '@material-ui/icons/Settings';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import VideocamOffIcon from '@material-ui/icons/VideocamOff';
-import { ChromeReaderMode } from '@material-ui/icons';
 import { setStorage } from '../../public/chrome/scripts/background';
+import axios from 'axios';
+
+import { client_id, token } from '../../public/config';
 
 interface IProps {
 	handleChange: (page: string) => void;
 }
 
 export default class Footer extends React.Component<IProps> {
+	getFollowing(user_id: string) {
+		axios
+			.get('https://api.twitch.tv/helix/users/follows', {
+				params: { from_id: user_id },
+				headers: {
+					'Client-Id': client_id,
+					Authorization: 'Bearer ' + token,
+				},
+			})
+			.then((res) => {
+				console.log(res.data.data);
+				console.log('Just sent a request for the user: ' + user_id);
+				setStorage(res.data.data.map((channel) => channel.to_name));
+			})
+			.catch((e) => console.error(e));
+	}
+
 	render() {
 		return (
 			<footer style={{ backgroundColor: '#FFF' }}>
@@ -27,10 +46,7 @@ export default class Footer extends React.Component<IProps> {
 					</Button>
 					<Button
 						onClick={() => {
-							console.log(
-								'I will implement this at some point I promise'
-							);
-							setStorage(['jamesinaxx', 'kinzixx', 'ludwig']);
+							this.getFollowing('538134305');
 						}}>
 						<SettingsIcon />
 					</Button>
