5ae285e8526bb43ce8b458dfa649e5a4f585b767
jamesinaxx <grassojames5@gmail.com>
2021-05-12 20:04:36 +1000

    Major updates... Got it working again for starters

--- public/chrome/scripts/background.js
+++ public/chrome/scripts/background.js
@@ -1,37 +1,50 @@
-import axios from 'axios';
-import { client_id, token } from '../../public/config';
-import { getStorage, setStorageLocal } from '../../../src/lib/chromeapi';
+const client_id = 'r69gvllvchvhsh0zvmg8i518a8zf7e';
+const token = 'h0qztx37s7ffbe3onmm45lr4b2h5fu';
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(async () => {
 	console.log('Initialized chrome extension');
 });
 
-chrome.storage.onChanged.addListener(() => getChannelInfo);
+chrome.storage.onChanged.addListener(async () => getChannelInfo);
 
-function getChannelInfo() {
-	getStorage('user').then((userName) =>
-		axios
-			.get('https://api.twitch.tv/helix/users', {
-				params: { login: userName },
-				headers: {
-					'Client-Id': client_id,
-					Authorization: 'Bearer ' + token,
-				},
-			})
-			.then((resa) => {
-				console.log('Made first http req to Twitch');
-				axios
-					.get('https://api.twitch.tv/helix/streams/followed', {
-						params: {
-							user_id: resa.data.data[0].id,
-						},
+function getStorage(key) {
+	return new Promise((resolve) => {
+		chrome.storage.sync.get([key], (res) => {
+			console.log(`Got the ${key} array: ` + res[key]);
+			resolve(res[key]);
+		});
+	});
+}
+
+function setStorageLocal(key, value) {
+	chrome.storage.local.set({ [key]: value }, () => {
+		console.log(`Set the ${key} array to ` + value);
+	});
+}
+
+async function getChannelInfo() {
+	getStorage('user').then(async (userName) =>
+		fetch('https://api.twitch.tv/helix/users?login=' + userName, {
+			headers: {
+				'Client-Id': client_id,
+				Authorization: 'Bearer ' + token,
+			},
+		}).then(async (resa) => {
+			console.log('Made first http req to Twitch');
+			resa.json().then((resjson) => {
+				console.log(resjson);
+				fetch(
+					'https://api.twitch.tv/helix/streams/followed?user_id=' +
+						resjson.data[0].id,
+					{
 						headers: {
 							'Client-Id': client_id,
 							Authorization: 'Bearer ' + token,
-							Accept: 'application/json',
 						},
-					})
-					.then((resb) => {
+					}
+				)
+					.then(async (resb) => {
+						const json = await resb.json();
 						console.log('Made second http req to Twitch');
 						console.log(
 							'Just sent a request for the user: ' + '538134305'
@@ -39,20 +52,24 @@ function getChannelInfo() {
 
 						console.log(
 							'This should be something like this idfk ' +
-								resb.data.data
+								json.data
 						);
 
-						setStorageLocal('channels', resb.data.data);
+						chrome.browserAction.setBadgeText({
+							text: json.data.length.toString(),
+						});
+
+						setStorageLocal('channels', json.data);
 					})
 					.catch((e) => console.error(e));
-			})
+			});
+		})
 	);
 }
 
-function getChannelInfoInit() {
-	return setTimeout(getChannelInfo, 60000);
+async function getChannelInfoInit() {
+	getChannelInfo();
+	return setInterval(async () => getChannelInfo(), 60000);
 }
 
 getChannelInfoInit();
-
-module.exports = getChannelInfo;
