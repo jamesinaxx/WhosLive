4fe455d0a37079417fd5a36ab8cdbaafba5762a3
jamesinaxx <grassojames5@gmail.com>
2021-06-23 01:01:18 +1000

    Updates, formatted some code and added webstorm ide files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # WhosLive
+
 Who's Live Now? A simple chrome extension that checks who is online
 
 ## NOTE: CURRENTLY IN BETA! THERE WILL BE BUGS AND MORE FEATURES ARE COMING SOON!
--- public/chrome/scripts/background.d.ts
+++
@@ -1,7 +0,0 @@
-declare function getChannelInfo(): NodeJS.Timeout;
-
-declare function setStorage(
-	key: string,
-	value: string,
-	callback: (key: string, value: any) => void
-): NodeJS.Timeout;
