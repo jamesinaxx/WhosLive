50dfd8ab7deab88560e6c314302bcae1233b78cf
jamesinaxx <grassojames5@gmail.com>
2021-06-26 11:23:25 +1000

    Fixed prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,7 +18,7 @@ const config = {
 	mode: 'production',
 	entry: {
 		index: './src/index.tsx',
-		'scripts/background': './src/scripts/background.js',
+		'scripts/background': './src/scripts/background.ts',
 	},
 	output: {
 		filename: '[name].js',
@@ -47,18 +47,18 @@ const config = {
 		new MiniCssExtractPlugin(),
 		new ESLintPlugin({ extensions }),
 		new HtmlWebpackPlugin({
-			template: 'public/index.html',
+			template: 'src/index.html',
 		}),
 		new CopyPlugin({
 			patterns: [
 				{
 					from: 'src/manifest.json',
 				},
-				{ from: 'public/chrome/icons', to: 'icons' },
-				{
-					from: 'public/chrome/scripts/background.js',
-					to: 'scripts',
-				},
+				{ from: 'src/icons', to: 'icons' },
+				// {
+				// 	from: 'public/chrome/scripts/background.js',
+				// 	to: 'scripts',
+				// },
 			],
 		}),
 		new Dotenv({ path: './.env' }),
