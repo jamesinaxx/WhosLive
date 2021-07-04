1da1bd2daa5fb78edb3ff1e508d999ef916366ad
jamesinaxx <grassojames5@gmail.com>
2021-07-04 21:01:56 +1000

    Removed old code

--- src/components/buttons/ColorModeToggle.tsx
+++ src/components/buttons/ColorModeToggle.tsx
@@ -1,28 +1,7 @@
 import React from 'react';
 import styles from '@styles/layout.module.scss';
-import { DarkModeIcon, LightModeIcon } from '@components/Icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons';
-
-interface ToggleProps {
-	icon: string;
-}
-
-// function ColorToggleIcon({ icon }: ToggleProps) {
-// 	return (
-// 		<>
-// 			<LightModeIcon
-// 				style={{
-// 					opacity: icon === 'light' ? '100%' : '0%',
-// 					position: 'absolute',
-// 				}}
-// 			/>
-// 			<DarkModeIcon
-// 				style={{ opacity: icon === 'dark' ? '100%' : '0%' }}
-// 			/>
-// 		</>
-// 	);
-// }
 
 interface SettingsButtonProps {
 	toggleColor: () => void;
