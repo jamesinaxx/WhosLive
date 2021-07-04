386b278b44337d45e50084a80a255943e7b3095c
jamesinaxx <grassojames5@gmail.com>
2021-07-04 21:01:36 +1000

    Updated some things

--- src/components/buttons/ColorModeToggle.tsx
+++ src/components/buttons/ColorModeToggle.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import styles from '@styles/layout.module.scss';
 import { DarkModeIcon, LightModeIcon } from '@components/Icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons';
 
 interface ToggleProps {
 	icon: string;
 }
 
-function ColorToggleIcon({ icon }: ToggleProps) {
-	return (
-		<>
-			<LightModeIcon
-				style={{
-					opacity: icon === 'light' ? '100%' : '0%',
-					position: 'absolute',
-				}}
-			/>
-			<DarkModeIcon
-				style={{ opacity: icon === 'dark' ? '100%' : '0%' }}
-			/>
-		</>
-	);
-}
+// function ColorToggleIcon({ icon }: ToggleProps) {
+// 	return (
+// 		<>
+// 			<LightModeIcon
+// 				style={{
+// 					opacity: icon === 'light' ? '100%' : '0%',
+// 					position: 'absolute',
+// 				}}
+// 			/>
+// 			<DarkModeIcon
+// 				style={{ opacity: icon === 'dark' ? '100%' : '0%' }}
+// 			/>
+// 		</>
+// 	);
+// }
 
 interface SettingsButtonProps {
 	toggleColor: () => void;
@@ -38,7 +40,7 @@ export default function SettingsButton({
 			className={styles.colorModeToggle}
 			onClick={toggleColor}
 			style={{ opacity: shown ? '0%' : '100%' }}>
-			<ColorToggleIcon icon={mode} />
+			<FontAwesomeIcon icon={mode === 'light' ? faSun : faMoon} />
 		</button>
 	);
 }
