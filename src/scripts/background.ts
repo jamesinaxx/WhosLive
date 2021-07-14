c843d6a311e26cb6bce82452bccc130385151184
jamesinaxx <grassojames5@gmail.com>
2021-07-15 09:14:07 +1000

    Fixed regen runtime

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,12 +1,13 @@
 import 'regenerator-runtime/runtime';
 const client_id = process.env.CLIENTID || '';
 import { setStorage, getChannelInfo } from '@lib/chromeapi';
+import 'regenerator-runtime/runtime';
 
 chrome.alarms.create({ delayInMinutes: 1 });
 
-function twitchtoken(): Promise<string | undefined> {
+async function twitchtoken(): Promise<string | undefined> {
 	return new Promise(resolve =>
-		chrome.storage.sync.get(['twitchtoken'], res => {
+		chrome.storage.sync.get(['twitchtoken'], async res => {
 			if (res.twitchtoken === undefined) {
 				chrome.storage.sync.set({ twitchtoken: undefined });
 				resolve(undefined);
@@ -30,7 +31,9 @@ chrome.runtime.onMessage.addListener(async message => {
 	console.log(message);
 });
 
-getChannelInfo(client_id, twitchtoken);
-chrome.alarms.onAlarm.addListener(async () => {
+(async () => {
 	getChannelInfo(client_id, twitchtoken);
-});
+	chrome.alarms.onAlarm.addListener(async () => {
+		getChannelInfo(client_id, twitchtoken);
+	});
+})();
