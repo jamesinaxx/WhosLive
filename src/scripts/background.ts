5ecc2f61b5ec3a84adad806438ae34f1163d29a5
jamesinaxx <grassojames5@gmail.com>
2021-07-15 10:23:22 +1000

    Got it working

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,7 +1,6 @@
 import 'regenerator-runtime/runtime';
 const client_id = process.env.CLIENTID || '';
-import { setStorage, getChannelInfo } from '@lib/chromeapi';
-import validateToken from '@/lib/tokenValid';
+import { setStorage, getChannelInfo, getStorage } from '@lib/chromeapi';
 
 async function twitchtoken(): Promise<string | undefined> {
 	return new Promise(resolve =>
@@ -16,29 +15,32 @@ async function twitchtoken(): Promise<string | undefined> {
 	);
 }
 
-chrome.alarms.create({ delayInMinutes: 1 });
+chrome.alarms.create('NowLiveRefresh', { delayInMinutes: 1 });
 
 chrome.runtime.onInstalled.addListener(async () => {
 	setStorage('mode', 'dark');
 	console.log('Initialized Now Live');
 });
 
-chrome.storage.onChanged.addListener(async () =>
-	getChannelInfo(client_id, twitchtoken)
-);
+chrome.storage.onChanged.addListener(async () => {
+	getChannelInfo(client_id, twitchtoken);
+	console.log('Token set to', await getStorage('twitchtoken'));
+});
 
 chrome.runtime.onMessage.addListener(async message => {
 	if (typeof message === 'object') {
 		if (message.name !== undefined && message.token !== undefined) {
 			console.log(message.token);
-			await validateToken(message.token);
+			setStorage('twitchtoken', message.token);
 		}
 	}
 });
 
 (async () => {
 	getChannelInfo(client_id, twitchtoken);
-	chrome.alarms.onAlarm.addListener(async () => {
-		getChannelInfo(client_id, twitchtoken);
+	chrome.alarms.onAlarm.addListener(async alarm => {
+		if (alarm.name === 'NowLiveRefresh') {
+			getChannelInfo(client_id, twitchtoken);
+		}
 	});
 })();
