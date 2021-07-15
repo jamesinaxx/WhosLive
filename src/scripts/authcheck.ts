2055bdbbc1b2cdb03fcd9f437a76ee2f68ff0538
jamesinaxx <grassojames5@gmail.com>
2021-07-15 11:25:04 +1000

    Updated content script

--- src/scripts/authcheck.ts
+++ src/scripts/authcheck.ts
@@ -11,11 +11,19 @@ window.location.hash = '';
 
 const authParent = authEl?.parentElement?.parentElement;
 
-chrome.runtime.sendMessage({
-	name: 'NowLiveAuthToken',
-	token: result.access_token,
-});
+chrome.runtime.sendMessage(
+	{
+		name: 'NowLiveAuthToken',
+		token: result.access_token,
+	},
+	(res: [string, boolean]) => {
+		const [msg, success] = res;
 
-if (authParent !== null && authParent !== undefined) {
-	authParent.innerText = 'Thank you :) Re-open NowLive to start using it';
-}
+		if (authParent !== null && authParent !== undefined) {
+			authParent.innerHTML = success
+				? '<h1>Thank you :) Re-open NowLive to start using it</h1>'
+				: '<h1>We tried to send the token, but something went wrong... Please let me know <a href="https://github.com/jamesinaxx/NowLive/issues">here</a></h1>';
+		}
+		console.log(`Received ${msg} in response to message`);
+	}
+);
