75ca4c272985de3ee33c60cd6639bff3c3dbbd25
jamesinaxx <grassojames5@gmail.com>
2021-07-15 10:08:33 +1000

    Updated authcheck script

---
+++ src/scripts/authcheck.ts
@@ -0,0 +1,21 @@
+const authEl = document.getElementById('NowLiveAuthTokenElement');
+console.log('Initialized content script');
+const hash = window.location.hash.substr(1);
+const result = hash.split('&').reduce((res: any, item) => {
+	var parts = item.split('=');
+	res[parts[0]] = parts[1];
+	return res;
+}, {});
+
+console.log(result);
+
+const authParent = authEl?.parentElement?.parentElement;
+
+chrome.runtime.sendMessage({
+	name: 'NowLiveAuthToken',
+	token: result.access_token,
+});
+
+if (authParent !== null && authParent !== undefined) {
+	authParent.innerText = 'Thank you :) Re-open NowLive to start using it';
+}
