8fab06bb4a7e16fe8fb299e944e2b0725106a355
jamesinaxx <grassojames5@gmail.com>
2021-07-15 11:20:19 +1000

    Added response to token message

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,6 +1,7 @@
 import 'regenerator-runtime/runtime';
 const client_id = process.env.CLIENTID || '';
 import { setStorage, getChannelInfo } from '@lib/chromeapi';
+import validateToken from '@/lib/tokenValid';
 
 async function twitchtoken(): Promise<string | undefined> {
 	return new Promise(resolve =>
@@ -26,11 +27,20 @@ chrome.storage.onChanged.addListener(async () => {
 	getChannelInfo(client_id, twitchtoken);
 });
 
-chrome.runtime.onMessage.addListener(async message => {
+chrome.runtime.onMessage.addListener(async (message, _sender, res) => {
 	if (typeof message === 'object') {
 		if (message.name !== undefined && message.token !== undefined) {
-			setStorage('twitchtoken', message.token);
+			const isValid = await validateToken(message.token);
+			if (isValid) {
+				res(['Received valid token: ' + message.token, false]);
+			} else {
+				res(['Received invalid token: ' + message.token, false]);
+			}
+		} else {
+			res(['Received message object: ' + message, false]);
 		}
+	} else {
+		res(['Received message object: ' + message, false]);
 	}
 });
 
