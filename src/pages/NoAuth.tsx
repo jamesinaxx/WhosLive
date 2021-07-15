ef6d4b6d2e9137245d24f7c85977d75f66d224ef
jamesinaxx <grassojames5@gmail.com>
2021-07-15 10:30:59 +1000

    Removed props from NoAuth page

--- src/pages/NoAuth.tsx
+++ src/pages/NoAuth.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import styles from '@styles/layout.module.scss';
 import 'regenerator-runtime/runtime';
 
-interface NoAuthProps {
-	colorMode: 'light' | 'dark';
-}
-
-export default function NoAuth({ colorMode }: NoAuthProps) {
+export default function NoAuth() {
 	return (
 		<small className={styles.noAuth}>
 			You are not logged in to Twitch! Please go to{' '}
