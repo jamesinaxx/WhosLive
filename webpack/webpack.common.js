0b8f98fc35a76df2e54e78d0620a7edc1f71dd0e
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-22 12:38:45 +1000

    Removed webcheck script

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -10,7 +10,6 @@ const config = {
 	entry: {
 		index: path.resolve(__dirname, '..', 'src/index.tsx'),
 		background: path.resolve(__dirname, '..', 'src/scripts/background.ts'),
-		webcheck: path.resolve(__dirname, '..', 'src/scripts/webcheck.ts'),
 		authcheck: path.resolve(__dirname, '..', 'src/scripts/authcheck.ts'),
 	},
 	output: {
