6c69fbc78aeb409134d54d2861388f7fdfef8c97
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-02 13:44:15 +1000

    Fixed the token valiidate script

--- src/lib/tokenValid.ts
+++ src/lib/tokenValid.ts
@@ -1,18 +1,22 @@
 import { setStorage } from '@lib/chromeapi';
 
 export default async function validateToken(token: string): Promise<boolean> {
-	const res = await (
-		await fetch('https://id.twitch.tv/oauth2/validate', {
-			headers: {
-				Authorization: `OAuth ${token}`,
-			},
-		})
-	).json();
+	try {
+		const res = await (
+			await fetch('https://id.twitch.tv/oauth2/validate', {
+				headers: {
+					Authorization: `OAuth ${token}`,
+				},
+			})
+		).json();
 
-	if (res.scopes.includes('user:read:follows') && res.expires_in > 500) {
-		setStorage('NowLive:Storage:Token', token);
-		return true;
-	} else {
+		if (res.scopes.includes('user:read:follows') && res.expires_in > 500) {
+			setStorage('NowLive:Storage:Token', token);
+			return true;
+		} else {
+			return false;
+		}
+	} catch (e) {
 		return false;
 	}
 }
