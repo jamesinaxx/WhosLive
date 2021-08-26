e6c11656ac6f4050a81523bc8d897a21d859ac3c
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-26 15:02:22 +1000

    Removed comment

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -16,11 +16,6 @@ const config = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    // alias: {
-    //   react: 'preact/compat',
-    //   'react-dom/test-utils': 'preact/test-utils',
-    //   'react-dom': 'preact/compat',
-    // },
   },
   module: {
     rules: [
