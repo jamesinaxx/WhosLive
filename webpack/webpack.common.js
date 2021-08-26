5d0f051a6315b22dd2ff8d67430339cfb56599bb
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-26 15:01:55 +1000

    Updated webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -16,11 +16,11 @@ const config = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      react: 'preact/compat',
-      'react-dom/test-utils': 'preact/test-utils',
-      'react-dom': 'preact/compat',
-    },
+    // alias: {
+    //   react: 'preact/compat',
+    //   'react-dom/test-utils': 'preact/test-utils',
+    //   'react-dom': 'preact/compat',
+    // },
   },
   module: {
     rules: [
