c51ea7488c1b96da87e0b0992ec508310eae5abf
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-31 09:55:11 +1000

    Removed html template and moved to inline template in webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -22,7 +22,19 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      // prettier-ignore
+      templateContent:
+      `<html>
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title></title>
+        </head>
+        <body>
+          <script src="index.js"></script>
+        </body>
+      </html>`,
       minify: true,
       inject: false,
     }),
