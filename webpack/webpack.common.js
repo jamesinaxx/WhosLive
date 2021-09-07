13a9a547aaf07374d792c8ae6254a64fd4d42bb2
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-09-07 22:05:46 +1000

    Fixed preact alias

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -16,6 +16,12 @@ const config = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat', // Must be below test-utils
+      'react/jsx-runtime': 'preact/jsx-runtime',
+    },
   },
   module: {
     rules,
