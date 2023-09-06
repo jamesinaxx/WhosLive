81c038424e6768a9aad7fdf2264ce6b942d60b52
Juliette Cordor <juliette@jewelexx.com>
2023-09-06 19:34:23 +1000

    use one list of favourites that is sorted

--- src/pages/Live.tsx
+++ src/pages/Live.tsx
@@ -49,39 +49,50 @@ const Live: FunctionComponent<PropsWithChildren<unknown>> = () => {
     });
   }, []);
 
+  const sortChannels = useCallback(
+    (left: TwitchStream, right: TwitchStream): number => {
+      const leftFave = favoriteChannels.has(left.user_id);
+      const rightFave = favoriteChannels.has(right.user_id);
+
+      if (leftFave && !rightFave) {
+        return -1;
+      }
+      if (!leftFave && rightFave) {
+        return 1;
+      }
+
+      if (left.viewer_count < right.viewer_count) {
+        return 1;
+      }
+      if (left.viewer_count > right.viewer_count) {
+        return -1;
+      }
+      return 0;
+    },
+    [favoriteChannels],
+  );
+
   if (channels === undefined) {
     return null;
   }
 
   return (
     <Container>
-      {channels.length === 0 && <NoLiveChannels />}
-      {[...favoriteChannels].map((channelName) => {
-        const channel = channels.find((c) => c.user_id === channelName);
-
-        if (channel === undefined) return null;
-
-        return (
-          <Channel
-            key={channel.id}
-            data={channel}
-            hidden={isLoading}
-            favorite
-            setFavorites={() => toggleFavorite(channel.user_id)}
-          />
-        );
-      })}
-      {channels
-        .filter((channel) => !favoriteChannels.has(channel.user_id))
-        .sort()
-        .map((channel) => (
-          <Channel
-            key={channel.id}
-            data={channel}
-            hidden={isLoading}
-            setFavorites={() => toggleFavorite(channel.user_id)}
-          />
-        ))}
+      {channels.length === 0 ? (
+        <NoLiveChannels />
+      ) : (
+        channels
+          .sort(sortChannels)
+          .map((channel) => (
+            <Channel
+              key={channel.id}
+              data={channel}
+              hidden={isLoading}
+              favorite={favoriteChannels.has(channel.user_id)}
+              setFavorites={() => toggleFavorite(channel.user_id)}
+            />
+          ))
+      )}
     </Container>
   );
 };
