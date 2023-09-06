4d66c6bcdfb71261c3560e669a48b27847dc47ae
Juliette Cordor <juliette@jewelexx.com>
2023-09-06 19:46:57 +1000

    fixes

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,14 +1,9 @@
 /* eslint-disable no-console */
-const dev = process.env.PRODUCTION !== 'true';
 
 export function log(...data: unknown[]): void {
-  if (dev) {
     console.log(...data);
-  }
 }
 
 export function error(...data: unknown[]): void {
-  if (dev) {
     console.error(...data);
-  }
 }
