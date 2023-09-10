f2eec5893817b1cc8b385944d18d7d848db25392
Juliette Cordor <juliette@jewelexx.com>
2023-09-10 19:21:51 +1000

    added comment

--- src/lib/chromeapi.ts
+++ src/lib/chromeapi.ts
@@ -122,6 +122,7 @@ export async function getChannelInfo(): Promise<void> {
       withicons.map(async (stream) => {
         const url = stream.profile_image_url;
         if (url.startsWith('https://static-cdn.jtvnw.net/')) {
+          // TODO: Maybe move to wasm for some of this
           const blob = await (await fetch(stream.profile_image_url)).blob();
           const base64Url = await blobToBase64(blob);
           const col = await fac.getColorAsync(base64Url, {
