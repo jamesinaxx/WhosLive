8a32ac1049a8d90384b10a5f3ec2727e941ba84f
Juliette Cordor <juliette@jewelexx.com>
2023-09-10 19:45:25 +1000

    added set panic hoo

--- image-helpers/src/lib.rs
+++ image-helpers/src/lib.rs
@@ -6,6 +6,7 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 /// Downloads the specified image url and converts it into a base64 url
 pub async fn download_image(url: String) -> String {
+    utils::set_panic_hook();
     let download = reqwest::get(url).await.unwrap().bytes().await.unwrap();
     BASE64_URL_SAFE.encode(download)
 }
