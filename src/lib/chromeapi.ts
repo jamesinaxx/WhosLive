52aa3973a63e75f24f44199e434560c571d3ec43
Juliette Cordor <juliette@jewelexx.com>
2023-09-15 20:05:23 +1000

    removed unused js function

--- src/lib/chromeapi.ts
+++ src/lib/chromeapi.ts
@@ -60,15 +60,6 @@ export async function setStorageLocalIfNull(
   }
 }
 
-const blobToBase64 = (blob: Blob): Promise<string> => {
-  const reader = new FileReader();
-  return new Promise<string>((resolve, reject) => {
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = reject;
-    reader.readAsDataURL(blob);
-  });
-};
-
 export async function getChannelInfo(): Promise<void> {
   const token = await getStorage('NowLive:Token');
   if (!token) {
