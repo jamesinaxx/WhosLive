f6da4e772c98f665921bf72947cb92cab4f8ecb6
Juliette Cordor <juliette@jewelexx.com>
2023-09-15 20:35:20 +1000

    add average colour finding

--- image-helpers/src/lib.rs
+++ image-helpers/src/lib.rs
@@ -8,6 +8,7 @@ macro_rules! log {
 }
 
 use base64::prelude::*;
+use image::{DynamicImage, GenericImageView};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -32,5 +33,55 @@ impl Image {
 
     pub fn to_base64(&self) -> String {
         BASE64_URL_SAFE.encode(&self.bytes)
+    }
+
+    pub fn average_color(&self) -> Colour {
+        let image = image::load_from_memory(&self.bytes).expect("valid image loaded");
+        Colour::sqrt_algorithm(image)
+    }
+}
+
+#[wasm_bindgen]
+pub struct Colour {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+    pub alpha: u8,
+}
+
+impl Colour {
+    pub fn new(colours: [u8; 4]) -> Self {
+        let [red, green, blue, alpha] = colours;
+
+        Self {
+            red,
+            green,
+            blue,
+            alpha,
+        }
+    }
+
+    fn sqrt_algorithm(image: DynamicImage) -> Self {
+        let pixels = image.pixels();
+        let mut red_total: u32 = 0;
+        let mut green_total: u32 = 0;
+        let mut blue_total: u32 = 0;
+        let mut alpha_total: u32 = 0;
+        let count = image.width() * image.height();
+
+        for pixel in pixels {
+            let [red, green, blue, alpha] = pixel.2 .0;
+            red_total += red as u32;
+            green_total += green as u32;
+            blue_total += blue as u32;
+            alpha_total += alpha as u32;
+        }
+
+        let red = red_total / alpha_total;
+        let blue = blue_total / alpha_total;
+        let green = green_total / alpha_total;
+        let alpha = alpha_total / count;
+
+        Self::new([red as u8, green as u8, blue as u8, alpha as u8])
     }
 }
